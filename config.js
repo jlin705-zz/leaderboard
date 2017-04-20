5ca60f93f8a2c4bbee63fb523e76646dbd94adfa
Jialiang Lin <jialiangl@zillowgroup.com>
2017-04-20 11:32:51 -0700

    add prod config

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ export default {
   searchEngineId: '008609127627542274017:bfdmtkhyyto',
   searchEngineApiKey: 'AIzaSyBpZ9ydOtT9vMfY2AaFi4ang2gSjP_avUY',
   giphyPublicKey: 'dc6zaTOxFJmzC',
-  mongodbUri: 'mongodb://localhost:27017/leaderboarddb',
+  mongodbUri: 'mongodb://leaderboard:admin@ds111851.mlab.com:11851/heroku_qsbxbj8n',
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
